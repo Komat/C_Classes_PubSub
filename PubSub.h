f2dba20c9b092425200901b9610944cfdc5b1a54
Komat <komatsu@komatsu.local>
2016-08-03 11:51:46 +0900

    add comment format

--- PubSub.h
+++ PubSub.h
@@ -14,17 +14,41 @@
 #include "../ItemList/ItemList.h"
 #include "../Dictionary/Dictionary.h"
 
-
+/**
+ *
+ */
 typedef void (*Subscriber)(void *context);
 
+/**
+ *
+ * @param event_name
+ * @param data
+ */
 void pubsub_publish(char *event_name, void *data);
 
+/**
+ *
+ * @param event_name
+ * @param fn
+ */
 void pubsub_subscribe(char *event_name, Subscriber fn);
 
+/**
+ *
+ * @param event_name
+ * @param fn
+ */
 void pubsub_unsubscribe(char *event_name, Subscriber fn);
 
+/**
+ *
+ * @return
+ */
 hash* pubsub_new();
 
+/**
+ *
+ */
 void pubsub_destory();
 
 
